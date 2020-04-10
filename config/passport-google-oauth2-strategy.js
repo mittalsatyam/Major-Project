ca55a3e4f831cc611806d4482f5e9f84e93f531b
satyam mittal <mittal.satyam026@gmail.com>
2020-04-11 00:47:34 +0530

    google authentication

---
+++ config/passport-google-oauth2-strategy.js
@@ -0,0 +1,82 @@
+// const passport=require('passport');
+// const googleStrategy=require('passport-google-oauth').OAuth2Strategy;
+// const crypto=require('crypto');
+// const User=require('../models/user');
+// //tell passport to use google strategy for google login
+// passport.use(new googleStrategy({
+// clientID:"264671854619-pi9qiiug03242bh2g2oi6e0751o94i2b.apps.googleusercontent.com",
+// clientSecret:"f2hh42pZxhEmzoSnA3pYJr8A",
+// callbackURL:"http://localhost:8000//users/auth/google/callback"
+// },
+// function(accessToken,refreshToken,profile,done){
+//     //find a user
+// User.findOne({email:profile.emails[0].value}).exec(function(err){
+// if(err){
+//     console.log("error occured",err);
+//     return;
+// }
+// console.log(profile);//if user find
+// if(user){
+//     return done(null,user);
+// }
+// else{//if not found create the user and set it in req.user
+//     User.create({
+//        name:profile.displayName,
+//        email:profile.emails[0].value,
+//        password:crypto.randomBytes(20).toString('hex') 
+//     },function(err,user){
+//     if(err){
+//         console.log("error",err);
+//         return;
+//        }
+//        return done(null,user);
+//      });
+//     }
+//  });
+// }
+// ));
+// module.exports=passport;
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const crypto = require('crypto');
+const User = require('../models/user');
+
+
+// tell passport to use a new strategy for google login
+passport.use(new googleStrategy({
+        clientID:"264671854619-pi9qiiug03242bh2g2oi6e0751o94i2b.apps.googleusercontent.com",
+        clientSecret: 'f2hh42pZxhEmzoSnA3pYJr8A', // e.g. _ASDFA%KFJWIASDFASD#FAD-
+        callbackURL: "http://localhost:8000/users/auth/google/callback",
+    },
+
+    function(accessToken, refreshToken, profile, done){
+        // find a user
+        User.findOne({email: profile.emails[0].value}).exec(function(err, user){
+            if (err){console.log('error in google strategy-passport', err); return;}
+            console.log(accessToken, refreshToken);
+            console.log(profile);
+
+            if (user){
+                // if found, set this user as req.user
+                return done(null, user);
+            }else{
+                // if not found, create the user and set it as req.user
+                User.create({
+                    name: profile.displayName,
+                    email: profile.emails[0].value,
+                    password: crypto.randomBytes(20).toString('hex')
+                }, function(err, user){
+                    if (err){console.log('error in creating user google strategy-passport', err); return;}
+
+                    return done(null, user);
+                });
+            }
+
+        }); 
+    }
+
+
+));
+
+
+module.exports = passport;
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo')(session);
+const passportGoogle = require('./config/passport-google-oauth2-strategy');
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
 
