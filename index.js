a471207fbb4bd47f13f4112ab77b3f5d0838aeeb
satyam mittal <mittal.satyam026@gmail.com>
2020-05-09 14:06:53 +0530

    deploys

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const env = require('./config/environment');
 const  logger=require('morgan');
 const cookieParser = require('cookie-parser');
 const app = express();
+require('./config/view-helpers')(app);
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
@@ -12,8 +13,17 @@ const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 //const passportJWT = require('./config/passport-jwt-strategy');
 const passportGoogle = require('./config/passport-google-oauth2-strategy');
-
 const MongoStore = require('connect-mongo')(session);
+const sassMiddleware =require('node-sass-middleware');
+if(env.name=='development'){
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+}));
+}
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
 
@@ -28,7 +38,8 @@ app.use(express.urlencoded());
 app.use(cookieParser());
 
 // app.use(express.static(env.asset_path));
-app.use(express.static('./assets'));
+//app.use(express.static('./assets'));
+app.use(express.static(env.asset_path));
 // make the uploads path available to the browser
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
